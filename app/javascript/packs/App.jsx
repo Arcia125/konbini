769d547dc34379a572ef0592b51f2e30d1037624
Kevin Hallett <kevinmhallett@gmail.com>
2020-09-05 10:51:30 -0500

    initial commit

---
+++ app/javascript/packs/App.jsx
@@ -0,0 +1,13 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>App React</div> at the bottom
+// of the page.
+
+import React from 'react';
+
+const App = (props) => (
+  <div>
+    <h1>Konbini</h1>
+  </div>
+);
+
+export default App;
